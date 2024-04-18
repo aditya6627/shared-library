7d425a273a6a60f47fafc0a760c80c12aaa0d08d
aditya <adityajambagi78@gmail.com>
2024-04-18 14:59:17 +0530

    added groovy

---
+++ vars/cicd.groovy
@@ -0,0 +1,17 @@
+def newGit(repo)
+{
+ git "${repo}"
+}
+def newMaven()
+{
+ sh 'mvn package'
+}
+def newDeploy(ip,appname)
+{
+deploy adapters: [tomcat9(credentialsId: '441b732f-3d44-4f64-b2d4-bf52559dc36b', path: '', url:"${ip}")], contextPath:"${appname}, war: '**/*.war'
+}
+def newTesting(jobname)
+{
+sh "java -jar /home/ubuntu/.jenkins/workspace/${jobname}/testing.jar"
+}
+
